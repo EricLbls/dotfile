38ba7714b0c31ab19474b50241a8e76ab3c3b0af
EricLbls <root@localhost.localdomain>
2017-05-25 17:35:09 +0800

    fixed install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # used for sonas_scripts
 ln -s ${BASEDIR}/.netrc ~/.netrc
 # zsh
-if [[ -f ~/.zshrc]] then
+if [ -f ~/.zshrc ]; then
         mv ~/.zshrc ~/.zshrc.local
 fi
 ln -s ${BASEDIR}/zsh/.zshrc ~/.zshrc
-if [[ -f ~/.zsh_history]] then
+if [ -f ~/.zsh_history ]; then
         mv ~/.zsh_history ~/.zsh_history_local
 fi
 ln -s ${BASEDIR}/zsh_history ~/.zsh_history
@@ -21,9 +21,12 @@ ln -s ${BASEDIR}/gitconfig ~/.gitconfig
 # tmux
 ln -s ${BASEDIR}/tmux/.tmux.conf ~/.tmux.conf
 # bash
+if [ -f ~/.bash_profile ]; then
+        mv ~/.bash_profile ~/.bash_profile.local
+fi
 ln -s ${BASEDIR}/bash_profile ~/.bash_profile
 # gnuplot
-ln -s ${BASEDIR}/gnuplot_history .gnuplot_history
+ln -s ${BASEDIR}/gnuplot_history ~/.gnuplot_history
 # vim
 ln -s ${BASEDIR}/vim ~/.vim
 ln -s ${BASEDIR}/vimrc ~/.vimrc
